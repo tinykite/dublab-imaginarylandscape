5cf9f8ccdea37cfad9af121bda6cc1d29db5bb52
tinykite <dakota@tinykitelab.com>
2020-02-02 12:46:47 -0600

    Fix safari podcast player bugs

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import AudioPlayerItem from './AudioPlayerItem';
+import React from "react";
+import AudioPlayerItem from "./AudioPlayerItem";
 
-const AudioPlayer = ({podcasts, ...props}) => (
-		<>
-		{podcasts && podcasts.map(({title, background, theme, year, city, location, time, audioUrl}, index) =>
-		<AudioPlayerItem key={'AudioPlayerItem-' + index} background={background} title={title} time={time} theme={theme} year={year} city={city} location={location} audioUrl={audioUrl} />
-		)}
-		</>
-)
+const AudioPlayer = ({ podcasts }) => (
+  <>
+    {podcasts &&
+      podcasts.map(
+        (
+          { title, background, theme, year, city, location, time, audioUrl },
+          index
+        ) => (
+          <AudioPlayerItem
+            key={"AudioPlayerItem-" + index}
+            background={background}
+            title={title}
+            time={time}
+            theme={theme}
+            year={year}
+            city={city}
+            location={location}
+            audioUrl={audioUrl}
+          />
+        )
+      )}
+  </>
+);
 
 export default AudioPlayer;
--- src/components/AudioPlayer/AudioPlayerItem.jsx
+++ src/components/AudioPlayer/AudioPlayerItem.jsx
@@ -1,69 +1,96 @@
-import React, {useState, useEffect, useRef} from 'react';
-import './AudioPlayer.scss';
-import PauseButton from './PauseButton.jsx';
-import PlayButton from './PlayButton.jsx';
-import { motion, useTransform } from 'framer-motion';
+import React, { useState, useEffect, useRef } from "react";
+import "./AudioPlayer.scss";
+import PauseButton from "./PauseButton.jsx";
+import PlayButton from "./PlayButton.jsx";
 
-const AudioPlayerItem = ({title, theme, background, year, city, location, time, audioUrl}) => {
-	const [playState, setPlayState] = useState(false); 
-	const [progressBar, setProgressBar] = useState(0);
-	const ref = useRef();
-	
-	const backgroundColor = {
-		background: `${background}`
-	}
+const AudioPlayerItem = ({
+  title,
+  theme,
+  background,
+  year,
+  city,
+  location,
+  time,
+  audioUrl
+}) => {
+  const [playState, setPlayState] = useState(false);
+  const [progressBar, setProgressBar] = useState(0);
+  const ref = useRef();
 
-	const onPlay = () => {
-		ref.current.play();
-		console.log("Playing");
-	}
+  const backgroundColor = {
+    background: `${background}`
+  };
 
-	const onPause = () => {
-		ref.current.pause();
-		console.log("Paused");
-	}
+  const onTimeUpdate = () => {
+    const duration = ref.current.duration;
+    let currentTime = ref.current.currentTime;
+    let progress = (currentTime / duration) * 100;
+    setProgressBar(progress);
+  };
 
-	const onTimeUpdate = () => {
-		const duration = ref.current.duration;
-		let currentTime = ref.current.currentTime;
-		let progress = (currentTime / duration) * 100;
-		setProgressBar(progress);
-		// const scrubber = `width: ${progressBar}%`;
-		console.log(progressBar);
-		// console.log(`${currentTime} of ${duration}, which is ${progress}%`);
-	}
+  const playAudio = () => {
+    setPlayState(!playState);
+    ref.current.play();
+  };
 
-	useEffect(() => {
-		if (playState) {
-			return onPause;
-		} else if (!playState) {
-			return onPlay;
-		}
-	}, [playState]);
+  const pauseAudio = () => {
+    setPlayState(!playState);
+    ref.current.pause();
+  };
 
-	return (
-		<section className={`podcast podcast--${theme}`} style={backgroundColor}>
-		<div className="podcast__player">
-		<audio controls src={audioUrl} ref={ref} onPlay={onPlay} onPause={onPause} onTimeUpdate={onTimeUpdate}/>
-		<motion.button className="podcast__button" onClick={() => setPlayState(!playState)}>
-			{playState ? (<PauseButton theme={theme} />) : (<PlayButton theme={theme} />)}
-    </motion.button>
-		<div class="podcast__main">
-		<h2 className="podcast__title">{title}</h2>
-		<div  className={`podcast__progressBar podcast__progressBar--${theme}`}>
-			<div style={{width: progressBar + "%"}} className={`podcast__progressBar-playing podcast__progressBar-playing--${theme}`}></div>
-		</div>
-			<ul className="podcast__meta">
-				<li className="podcast__metaItem">{year} |</li>
-				<li className="podcast__metaItem">{city} |</li>
-				<li className="podcast__metaItem">{time} |</li>
-				<li className="podcast__metaItem">{location}</li>
-			</ul>
-		</div>
-		</div>
-	</section>
-	)
-}
-
+  return (
+    <section className={`podcast podcast--${theme}`} style={backgroundColor}>
+      <div className="podcast__player">
+        <div className="podcast__main">
+          {playState ? (
+            <button className="podcast__button-container" onClick={pauseAudio}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 25 25"
+                width="25"
+                height="25"
+                className={`podcast__button podcast__button--${theme}`}
+              >
+                <rect x="0" y="0" width="6" height="25"></rect>
+                <rect x="13" y="0" width="6" height="25"></rect>
+              </svg>
+            </button>
+          ) : (
+            <button className="podcast__button-container" onClick={playAudio}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 25 25"
+                width="25"
+                height="25"
+                className={`podcast__button podcast__button--${theme}`}
+              >
+                <polygon points="0,0 25,12.5 0,25 0,0" />
+              </svg>
+            </button>
+          )}
+          <h2 className="podcast__title">{title}</h2>
+        </div>
+        <div className={`podcast__progressBar podcast__progressBar--${theme}`}>
+          <audio
+            src={audioUrl}
+            ref={ref}
+            onTimeUpdate={onTimeUpdate}
+            controls
+          />
+          <div
+            style={{ width: progressBar + "%" }}
+            className={`podcast__progressBar-playing podcast__progressBar-playing--${theme}`}
+          ></div>
+        </div>
+        <ul className="podcast__meta">
+          <li className="podcast__metaItem">{year} |</li>
+          <li className="podcast__metaItem">{city} |</li>
+          <li className="podcast__metaItem">{time} |</li>
+          <li className="podcast__metaItem">{location}</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default AudioPlayerItem;
